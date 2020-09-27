e54bf0a304db21ca747ced3f3f657f6e9a0d8ca0
Tom Sasser <tom_sasser@cable.comcast.com>
2020-09-27 23:37:13 +0300

    Added tests

--- main.go
+++ main.go
@@ -1,7 +1,17 @@
 package main
 
-import "github.com/tsasser05/dierollsvc"
+import (
+	"fmt"
+
+	"github.com/tsasser05/dieroll"
+)
 
 func main() {
-	dierollsvc.Dieroll()
+	dieroll.Init()
+	//dierollsvc.Example()
+	var numRolls int = 16
+	var myRolls = make([]int, numRolls)
+	//fmt.Println("Initialized slice:  ", myRolls)
+	myRolls = dieroll.Rolldice(10, numRolls)
+	fmt.Println("The results of the dice rolled:  ", myRolls)
 }
